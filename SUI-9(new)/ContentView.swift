0f3177c80d9cde39bb21d7092c19fcafa428d4c1
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-12-27 13:22:10 +0300

    Initial Commit

---
+++ SUI-9(new)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SUI-9(new)
+//
+//  Created by  Pavel Nepogodin on 27.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SUI-9(new)/SUI_9_new_App.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_9_new_App.swift
+//  SUI-9(new)
+//
+//  Created by  Pavel Nepogodin on 27.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SUI_9_new_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
