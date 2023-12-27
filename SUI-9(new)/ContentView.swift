be863acb00311de07ee7f1767a5a96cb9d35a9e8
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-12-27 13:24:41 +0300

    done

--- SUI-9(new)/ContentView.swift
+++ SUI-9(new)/ContentView.swift
@@ -8,19 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var position: CGSize = .zero
+    
+    func setPositionWithAnimation(_ position: CGSize) {
+        withAnimation(.interpolatingSpring(
+            stiffness: 150,
+            damping: 15
+        )) {
+            self.position = position
         }
-        .padding()
+    }
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.radialGradient(
+                    colors: [.yellow, .orange],
+                    center: .center,
+                    startRadius: 50,
+                    endRadius: 100
+                ))
+                .mask {
+                    Canvas { context, size in
+                        context.addFilter(.alphaThreshold(
+                            min: 0.5,
+                            color: .black
+                        ))
+                        context.addFilter(.blur(radius: 30))
+                        context.drawLayer { ctx in
+                            for index in [1,2] {
+                                if let shape = context.resolveSymbol(
+                                    id: index
+                                ) {
+                                    ctx.draw(shape, at: CGPoint(
+                                        x: size.width / 2,
+                                        y: size.height / 2
+                                    ))
+                                }
+                            }
+                        }
+                    } symbols: {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .tag(1)
+                        
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .offset(position)
+                            .tag(2)
+                    }
+                }
+            Image(systemName: "cloud.sun.rain.fill")
+                .foregroundStyle(Color.white)
+                .symbolRenderingMode(.hierarchical)
+                .font(.system(size: 30))
+                .frame(width: 100, height: 100)
+                .offset(position)
+        }
+        .background(.black)
+        .ignoresSafeArea()
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    setPositionWithAnimation(value.translation)
+                }).onEnded({ _ in
+                    setPositionWithAnimation(.zero)
+                })
+        )
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
